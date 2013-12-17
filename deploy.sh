a4eb61f4a4e20336b3090c4f7c9725842bdf0471
Drew <drew@gradientx.com>
2013-12-16 18:44:56 -0800

    Updating build stuff to match the new repos

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./sbt package make-pom
-mvn deploy:deploy-file -DpomFile=core/target/scala-2.8.0/kafka_2.8.0-0.8.0-SNAPSHOT.pom \
--Dfile=core/target/scala-2.8.0/kafka_2.8.0-0.8.0-SNAPSHOT.jar \
+mvn deploy:deploy-file -DpomFile=core/target/scala-2.8.0/kafka_2.8.0-0.8.1-SNAPSHOT.pom \
+-Dfile=core/target/scala-2.8.0/kafka_2.8.0-0.8.1-SNAPSHOT.jar \
 -DrepositoryId=gradientx-snapshot-repo \
--Durl=s3://codex.corp.gradientx.com/maven/snapshot
+-Durl=s3://prod.codex.gradientx.com/maven/snapshot
