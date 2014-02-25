386881d7df518ec38f6938840768a8b4aeb77656
Drew <drew@gradientx.com>
2014-02-25 14:29:08 -0800

    Modifying build to be a SNAPSHOT

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./sbt package make-pom
-mvn deploy:deploy-file -DpomFile=core/target/scala-2.8.0/kafka_2.8.0-0.8.1-SNAPSHOT.pom \
--Dfile=core/target/scala-2.8.0/kafka_2.8.0-0.8.1-SNAPSHOT.jar \
--DrepositoryId=gradientx-snapshot-repo \
--Durl=s3://prod.codex.gradientx.com/maven/snapshot
